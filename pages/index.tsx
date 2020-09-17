2ad8650d0bf8e6cb8b9372adc28fc36a6295c097
Tom Albrighton <thomas.albrighton@four.io>
2020-09-17 16:52:56 +0930

    Slight styling on the login page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,18 +5,40 @@ import RecommendList from '../components/RecommendList'
 
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
 import { useState } from 'react'
+import { createStyles, makeStyles } from '@material-ui/core'
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    loginContainer: {
+      height: '100vh'
+    },
+    loginModal: {
+      position: 'relative',
+      /* ↓ Push the element down 50% of the parent */
+      top: '50%',
+      /* ↓ Then adjust it by 50% of its own height */
+      transform: 'translateY(-50%)',
+      textAlign: 'center'
+    }
+  })
+)
 
 const IndexPage = () => {
   const [ session, loading ] = useSession()
   const [basedOn, setBasedOn] = useState(null)
+
+  const styles = useStyles()
   
   return <>
-    {!session && <>
-      Not signed in <br/>
-      <button onClick={signIn}>Sign in</button>
-    </>}
+    {!session &&
+      <div class={styles.loginContainer}>
+        <div class={styles.loginModal}>
+          <Button variant="contained" onClick={signIn}>Sign in</Button>
+        </div>
+      </div>
+    }
     {session && <>
       <Grid container spacing={3}>
         <Grid item xs={6}>
