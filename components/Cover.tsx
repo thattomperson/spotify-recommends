f30515327c9a7d982fdf2ade5ecb9073a3e245f7
Tom Albrighton <thomas.albrighton@four.io>
2022-07-02 09:55:10 +0930

    Replace JSX.ElementChildrenAttribute with React.ReactNode

--- components/Cover.tsx
+++ components/Cover.tsx
@@ -1,8 +1,9 @@
 interface CoverProps {
   className?: string
+  children?: React.ReactNode;
 }
 
-const Cover = ({ children, className }: JSX.ElementChildrenAttribute & CoverProps): JSX.Element => {
+const Cover = ({ children, className }: CoverProps): JSX.Element => {
   return (
     <div className={`absolute inset-0 h-screen flex justify-center items-center ${className}`}>
       {children}
